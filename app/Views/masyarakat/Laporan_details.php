6deeddf6d4803e73743a62b5f276434e402d4622
I Made Ravindra P <ravinpraditya274@gmail.com>
2023-11-13 14:25:48 +0700

    Halaman laporan details

--- app/Views/masyarakat/Laporan_details.php
+++ app/Views/masyarakat/Laporan_details.php
@@ -8,15 +8,83 @@
             <div class="row">
                 <div class="col">
                     <div class="card">
+                        <div>
+                            <div style="
+                        width: 634px;
+                        height: 20px;
+                        flex-shrink: 0;
+                        border-radius: 5px 5px 0px 0px;
+                        background: #E83B3B;
+                  "></div>
+                        </div>
                         <div class="card-body">
-                            <p>testing</p>
+                            <div>
+                                <img style="width: 180px; height: 180px; flex-shrink: 0;" src="/kodingan/frontend/img/Group 1.png" alt="">
+                            </div>
+                            <div style="width: 573px; background: #000; height: 1px; margin-top: 25px;">
+
+                            </div>
+                            <div class="pt-3">
+                                <div class="row">
+                                    <div class="col-3">
+                                        <p>Nama Laporan</p>
+                                        <p>Tanggal laporan</p>
+                                        <p>Lokasi</p>
+                                        <p>Kategori</p>
+                                        <p>Isi laporan</p>
+                                    </div>
+                                    <div class="col-1" style="margin-left: -20px;">
+                                        <p>:</p>
+                                        <p>:</p>
+                                        <p>:</p>
+                                        <p>:</p>
+                                        <p>:</p>
+                                    </div>
+                                    <div class="col" style="margin-left: -30px;">
+                                        <p>Kerusuhan tahun 98</p>
+                                        <p>jakarta</p>
+                                        <p>Huru hara</p>
+                                        <p>Kerusuhan</p>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dolorem voluptatibus ea quo eos. Expedita modi beatae placeat, et rerum incidunt molestias doloremque nam nostrum ea debitis obcaecati iusto delectus!</p>
+                                    </div>
+                                </div>
+
+                            </div>
+
                         </div>
                     </div>
                 </div>
                 <div class="col">
                     <div class="card">
+                        <div>
+                            <div style="
+                        width: 634px;
+                        height: 20px;
+                        flex-shrink: 0;
+                        border-radius: 5px 5px 0px 0px;
+                        background: #E83B3B;
+                  "></div>
+                        </div>
                         <div class="card-body">
-
+                            <div class="mb-4">
+                                <h2>Tanggapan</h2>
+                            </div>
+                            <div>
+                                <div class="mb-3" style="width: 602px; height: 68px; flex-shrink: 0; border-radius: 10px; background: #FFCA7B; ">
+                                    <p class="pt-2" style="margin-left: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, numquam nobis. Quos solutat!</p>
+                                </div>
+                                <div class="mb-3" style="width: 602px; height: 68px; flex-shrink: 0; border-radius: 10px; background: #FFCA7B; ">
+                                    <p class="pt-2" style="margin-left: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, numquam nobis. Quos solutat!</p>
+                                </div>
+                                <div class="mb-3" style="width: 602px; height: 68px; flex-shrink: 0; border-radius: 10px; background: #FFCA7B; ">
+                                    <p class="pt-2" style="margin-left: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, numquam nobis. Quos solutat!</p>
+                                </div>
+                                <div class="mb-3" style="width: 602px; height: 68px; flex-shrink: 0; border-radius: 10px; background: #FFCA7B; ">
+                                    <p class="pt-2" style="margin-left: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, numquam nobis. Quos solutat!</p>
+                                </div>
+                            </div>
+                            
+                            
                         </div>
                     </div>
                 </div>
